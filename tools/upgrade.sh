3fed9e72a5986d7dad82b1e23acdc096f1ae3f17
gnomeye <gnomeye@gmail.com>
2012-09-23 00:45:30 +0200

    modified

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -2,8 +2,11 @@ current_path=`pwd`
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 cd $ZSH
 
-if git pull origin master
+if git fetch upstream
+#if git pull origin master
 then
+  git merge upstream/master
+  git pull origin master
   printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
   printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
   printf '\033[0;32m%s\033[0m\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
