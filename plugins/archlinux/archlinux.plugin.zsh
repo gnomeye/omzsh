b9240988d8cdabf690d814631d679c3617d70374
gnomeye <gnomeye@gmail.com>
2012-10-23 21:09:30 +0200

    few mods

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -20,7 +20,7 @@ if [[ -x `which yaourt` ]]; then
   alias yalocs='yaourt -Qs'        # Search for package(s) in the local database
   # Additional yaourt alias examples
   if [[ -x `which abs` ]]; then
-    alias yaupd='yaourt -Sy && abs'   # Update and refresh the local package and ABS databases against repositories
+    alias yaupd='yaourt -Sy && sudo abs'   # Update and refresh the local package and ABS databases against repositories
   else
     alias yaupd='yaourt -Sy'               # Update and refresh the local package and ABS databases against repositories
   fi
@@ -28,36 +28,36 @@ if [[ -x `which yaourt` ]]; then
   alias yamir='yaourt -Syy'                # Force refresh of all package lists after updating /etc/pacman.d/mirrorlist
 else
  upgrade() {
-   pacman -Syu
+   sudo pacman -Syu
  }
 fi
 
 # Pacman - https://wiki.archlinux.org/index.php/Pacman_Tips
-alias pacupg='pacman -Syu'        # Synchronize with repositories before upgrading packages that are out of date on the local system.
-alias pacin='pacman -S'           # Install specific package(s) from the repositories
-alias pacins='pacman -U'          # Install specific package not from the repositories but from a file 
-alias pacre='pacman -R'           # Remove the specified package(s), retaining its configuration(s) and required dependencies
-alias pacrem='pacman -Rns'        # Remove the specified package(s), its configuration(s) and unneeded dependencies
+alias pacupg='sudo pacman -Syu'        # Synchronize with repositories before upgrading packages that are out of date on the local system.
+alias pacin='sudo pacman -S'           # Install specific package(s) from the repositories
+alias pacins='sudo pacman -U'          # Install specific package not from the repositories but from a file 
+alias pacre='sudo pacman -R'           # Remove the specified package(s), retaining its configuration(s) and required dependencies
+alias pacrem='sudo pacman -Rns'        # Remove the specified package(s), its configuration(s) and unneeded dependencies
 alias pacrep='pacman -Si'              # Display information about a given package in the repositories
 alias pacreps='pacman -Ss'             # Search for package(s) in the repositories
 alias pacloc='pacman -Qi'              # Display information about a given package in the local database
 alias paclocs='pacman -Qs'             # Search for package(s) in the local database
 # Additional pacman alias examples
-#if [[ -x `which abs` ]]; then
-#  alias pacupd='pacman -Sy && abs'     # Update and refresh the local package and ABS databases against repositories
-#else
-#  alias pacupd='pacman -Sy'     # Update and refresh the local package and ABS databases against repositories
-#fi
-alias pacinsd='pacman -S --asdeps'        # Install given package(s) as dependencies of another package
-alias pacmir='pacman -Syy'                # Force refresh of all package lists after updating /etc/pacman.d/mirrorlist
+if [[ -x `which abs` ]]; then
+  alias pacupd='sudo pacman -Sy && sudo abs'     # Update and refresh the local package and ABS databases against repositories
+else
+  alias pacupd='sudo pacman -Sy'     # Update and refresh the local package and ABS databases against repositories
+fi
+alias pacinsd='sudo pacman -S --asdeps'        # Install given package(s) as dependencies of another package
+alias pacmir='sudo pacman -Syy'                # Force refresh of all package lists after updating /etc/pacman.d/mirrorlist
 
 # https://bbs.archlinux.org/viewtopic.php?id=93683
 paclist() {
-  pacman -Qei $(pacman -Qu|cut -d" " -f 1)|awk ' BEGIN {FS=":"}/^Name/{printf("\033[1;36m%s\033[1;37m", $2)}/^Description/{print $2}'
+  sudo pacman -Qei $(pacman -Qu|cut -d" " -f 1)|awk ' BEGIN {FS=":"}/^Name/{printf("\033[1;36m%s\033[1;37m", $2)}/^Description/{print $2}'
 }
 
-alias paclsorphans='pacman -Qdt'
-alias pacrmorphans='pacman -Rs $(pacman -Qtdq)'
+alias paclsorphans='sudo pacman -Qdt'
+alias pacrmorphans='sudo pacman -Rs $(pacman -Qtdq)'
 
 pacdisowned() {
   tmp=${TMPDIR-/tmp}/pacman-disowned-$UID-$$
